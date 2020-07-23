d16af290d6772b571b12747abb44a3fb9a138d46
wilder4wcs <jimmy.boucaud@gmail.com>
2020-07-23 22:36:26 +0200

    Faker added to Fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,34 +2,53 @@
 
 namespace App\DataFixtures;
 
+
 use App\Entity\Artist;
+use App\Entity\Category;
 use App\Entity\Performance;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        //Création de fixtures artist
-        for ($i = 1; $i <= 12; $i++) {
-            $artist = new Artist();
-            $artist->setName("Nom de l'Artiste")
-                ->setDescription("Une bio rapide")
-                ->setImage("http://palcehold.it/350X250");
-            $manager->persist($artist);
+
+        $faker = Factory::create('fr_FR');
+
+        //Création des 3 Catégories Fake pour les spectacles et les artistes
+       for($i = 1; $i <= 3; $i++){
+           $category = new Category();
+           $category->setName($faker->word());
+
+           $manager->persist($category);
+
+           //Création de 12 Artistes Fake
+           for ($j = 1; $j <= 4; $j++) {
+               $artist = new Artist();
+
+               $artist->setName($faker->name())
+                      ->setDescription($faker->text())
+                      ->setImage($faker->imageUrl())
+                      ->setCategoryId($category);
+               $manager->persist($artist);
+
+               //Création de fixtures shows
+               for ($k = 1; $k <= 3; $k++) {
+                   $performance = new Performance();
+
+                   $performance->setName($faker->word())
+                               ->setDescription($faker->text())
+                               ->setImage($faker->imageUrl)
+                               ->setShowedAt($faker->dateTimeBetween('now', '+2 years'))
+                               ->setCategoryId($category);
+                   $manager->persist($performance);
+
+       }
+
         }
 
-
-
-    //Création de fixtures shows
-    for ($i = 1; $i <= 9; $i++) {
-        $performance = new Performance();
-        $performance->setName("Nom du spectacle")
-            ->setDescription("Un rapide aperçu")
-            ->setImage("http://palcehold.it/350X250")
-            ->setShowedAt(new \DateTime());
-        $manager->persist($performance);
     }
     $manager->flush();
 }
